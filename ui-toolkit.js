5615c110b476e04f8e65f6803936494d64bfdb28
Michael Bazos <micabazos@gmail.com>
2017-05-30 17:20:01 +0200

    release

---
+++ ui-toolkit.js
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+class DropdownComponent {
+    constructor() { }
+}
+DropdownComponent.decorators = [
+    { type: Component, args: [{
+                selector: 'ui-dropdown',
+                templateUrl: './dropdown.component.html',
+                styleUrls: ['./dropdown.component.scss']
+            },] },
+];
+/**
+ * @nocollapse
+ */
+DropdownComponent.ctorParameters = () => [];
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { DropdownComponent };
